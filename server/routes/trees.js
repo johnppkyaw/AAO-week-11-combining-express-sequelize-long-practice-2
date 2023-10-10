d1c4da51f58ad5837f93612beeb822c8178dd30d
John <jjpyae@gmail.com>
2023-10-10 12:41:36 -0700

    completes bonus phase 1

--- server/routes/trees.js
+++ server/routes/trees.js
@@ -277,8 +277,17 @@ router.put('/:id', async (req, res, next) => {
  */
 router.get('/search/:value', async (req, res, next) => {
     let trees = [];
-
-
+    trees = await Tree.findAll({
+        attributes: [
+            'heightFt', 'tree', 'id'
+        ],
+        where: {
+            tree: {
+                [Op.like]:`%${req.params.value}%`
+            }
+        },
+        order: [['tree']]
+    })
     res.json(trees);
 });
 
